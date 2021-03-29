b9b6e56d9942c702a0d1eb67a834c62aaa3c83f6
FortuneZA <61013523+FortuneZA@users.noreply.github.com>
2021-03-29 22:03:03 +0200

    Add files via upload

---
+++ ADPassignment1Testing/src/main/java/za/ac/cput/App.java
@@ -0,0 +1,17 @@
+package za.ac.cput;
+
+/**
+ * Author: Mathew Fortuin - 219069514
+ *Main class
+ */
+public class App 
+{
+    public static int Calculator(int a,int b)
+    {
+        return a*b; //(Multiplication)
+    }
+    public static void main( String[] args )
+    {
+        System.out.println(  );
+    }
+}
