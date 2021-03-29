82ca9d245bc4dcb11ff68d585052f06bff7b4adf
FortuneZA <mfortuin509@gmail.com>
2021-03-29 21:32:42 +0200

    First commit of main and test file

---
+++ src/main/java/za/ac/cput/App.java
@@ -0,0 +1,13 @@
+package za.ac.cput;
+
+/**
+ * Author: Mathew Fortuin - 219069514
+ *Main class
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is a test!" );
+    }
+}
